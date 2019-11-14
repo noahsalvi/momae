85ef39e179f73426f8aafc4b5ff31d770fcee133
Nikita Rüegsegger <nru109721@iet-gibb.ch>
2019-11-14 08:15:01 +0100

    create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { CatalogComponent } from './catalog/catalog.component';
     AppComponent,
     HeaderComponent,
     CatalogComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
